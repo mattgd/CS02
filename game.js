c9085d727efc299d7a330d15db402bab92847231
mattgd <matt.dzwonczyk@live.com>
2015-05-27 19:48:32 -0400

    Began working on travel system.

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // Declare the variables
 var title = "Seven Day Tour";
-var money, textarea, textIn, textOut, transportationType;
-
+var location, money, nextLocation, textarea, textIn, textOut, transportationType;
+var gameStarted = false;
 /* Transportation Types
     0 - plane
     1 - boat
@@ -14,7 +14,15 @@ $(document).ready(function() {
     money = 7000;
     textIn = $('#input');
     textOut = $('#output');
+    textOut.val("Are you ready to begin? Type 'yes' or 'no' in the text box below.\n\n");
     textarea = document.getElementById("output");
+});
+
+$(document).keypress(function(e) {
+    if (e.which == 13) {
+        console.log("HERE");
+        $('#submit').click();
+    }
 });
 
 // What happens when the user clicks "Submit"
@@ -38,22 +46,52 @@ function process(input) {
     s = punctuationless.replace(/\s{2,}/g, " ");
     s = s.toLowerCase();
 
-    switch (s) {
-        case "reset":
-            reset();
-            s = "Game reset.";
-            break;
-        case "help":
-            s = "Here to help!";
-            break;
-        case "money":
-            s = "You have $"  + money + ".";
-            break;
-        case "status":
-            s = travelStatus();
-            break;
-        default:
-            s = "I'm afraid '" + s + "' is not allowed.";
+    if (gameStarted) {
+        switch (s) {
+            case "yes":
+                break;
+            case "no":
+                break;
+            case "reset":
+                reset();
+                s = "Game reset.";
+                break;
+            case "help":
+                s = "Here to help!";
+                break;
+            case "money":
+                s = "You have $"  + money + ".";
+                break;
+            case "status":
+                s = travelStatus();
+                break;
+            case "africa":
+            case "antarctica":
+            case "asia":
+            case "europe":
+            case "south america":
+            case "antarctica":
+                var location = s.substr(0, 1).toUpperCase() + s.substr(1);
+                setNextLocation(location);
+                s = location + ". Great! How would you like to get there?";
+                break;
+            case "plane":
+            case "boat":
+            case "car":
+                s = getTransportation(s);
+                break;
+            default:
+                s = "I'm afraid '" + s + "' is not allowed.";
+        }
+    } else {
+        if (s == "yes" || s == "y") {
+            gameStarted = true;
+            s = "You call a taxi. The taxi driver asks you where you're headed.";
+        } else if (s == "no" || s == "n") {
+            s = "Alright, that's fine.";
+        } else {
+            s = "It's a simple yes or no question.";
+        }
     }
 
     return s;
@@ -68,7 +106,6 @@ function reset() {
 }
 
 function travelStatus() {
-
     switch (transportationType) {
         case 0:
             // Plane
@@ -82,5 +119,45 @@ function travelStatus() {
         default:
             // Not moving
     }
+}
 
+function convertTransportationType(type) {
+    switch (type) {
+        case "plane":
+            type = 0;
+            break;
+        case "boat":
+            type = 3;
+            break;
+        case "car":
+            type = 2;
+            break;
+        default:
+            type = 3;
+    }
+    return type;
+}
+
+function getTransportation(type) {
+    var s;
+    type = convertTransportationType(type);
+    if (location == "North America") {
+        if (type == 2) {
+            s = "Sorry, you cannot drive to "
+        } else if (type == 0) {
+            s = "I'll drive you to the airport to fly to ";
+        } else if (type == 1) {
+            s = "I'll drive you to the port to sail to ";
+        } else {
+            s = "I'm sorry, you can't travel that way to ";
+        }
+    } else if {
+
+    }
+
+    return;
+}
+
+function setNextLocation(location) {
+    nextLocation = location;
 }
